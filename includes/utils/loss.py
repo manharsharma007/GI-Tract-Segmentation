f02f070740a8af3cb70031716eed6552d1e7440f
Manhar <manharsharma007@gmail.com>
2022-07-21 21:57:07 +0530

    Added new history

--- includes/utils/loss.py
+++ includes/utils/loss.py
@@ -102,20 +102,24 @@ class DiceLoss(nn.Module):
         return dice
 
 
-class FocalLoss(nn.CrossEntropyLoss):
-    ''' Focal loss for classification tasks on imbalanced datasets '''
+class TverskyLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(TverskyLoss, self).__init__()
 
-    def __init__(self, gamma, alpha=None, ignore_index=-100, reduction='none'):
-        super().__init__(weight=alpha, ignore_index=ignore_index, reduction='none')
-        self.reduction = reduction
-        self.gamma = gamma
-
-    def forward(self, input_, target):
-        cross_entropy = super().forward(input_, target)
-        # Temporarily mask out ignore index to '0' for valid gather-indices input.
-        # This won't contribute final loss as the cross_entropy contribution
-        # for these would be zero.
-        target = target * (target != self.ignore_index).long()
-        input_prob = torch.gather(F.softmax(input_, 1), 1, target.unsqueeze(1))
-        loss = torch.pow(1 - input_prob, self.gamma) * cross_entropy
-        return torch.mean(loss) if self.reduction == 'mean' else torch.sum(loss) if self.reduction == 'sum' else loss
+    def forward(self, inputs, targets, smooth=1, alpha=0.5, beta=0.5):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = torch.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum()    
+        FP = ((1-targets) * inputs).sum()
+        FN = (targets * (1-inputs)).sum()
+       
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)
+        
+        return (1 - Tversky)
