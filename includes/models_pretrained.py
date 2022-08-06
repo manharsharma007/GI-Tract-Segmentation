5d84b377879e56c92f81b1d1f02ed89fa1c7f7bf
Manhar Sharma <manharsharma007@gmail.com>
2022-08-06 12:28:39 +0530

    Update models_pretrained.py

--- includes/models_pretrained.py
+++ includes/models_pretrained.py
@@ -3,8 +3,8 @@ from .model_vanilla import UNET
 
 def get_unet_model(type_, inchannels, outchannels, weights = None, device = "cuda"):
 	if(type_ == "vanilla"):
-		return UNET(in_features=1, out_features=3).to(device)
+		return UNET(in_features=inchannels, out_features=outchannels).to(device)
 
 	elif(type_ in ["efficientnet-b1", "resnet34", "vgg16", "mobilenet_v2", "resnet50", "efficientnet-b3", "efficientnet-b5", "inceptionresnetv2"]):
 		return smp.Unet(encoder_name=type_, encoder_weights=weights,
-                         in_channels=1, classes=3).to(device)
+                         in_channels=inchannels, classes=outchannels).to(device)
